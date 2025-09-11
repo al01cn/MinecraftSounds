8d4e73009734a0c43e823b3d284c502263e5fc25
小阿狼是也 <Y_ALANG@yeah.net>
2025-09-11 20:46:21 +0800

    fix(utils): 修复移动端检测逻辑中浏览器变量未定义问题
    重构了浏览器用户代理的获取方式，避免在非浏览器环境中直接访问navigator导致错误。同时优化了代码格式，使其更易读。

--- .vitepress/utils/IsMobile.js
+++ .vitepress/utils/IsMobile.js
@@ -1,10 +1,11 @@
 const mobileAgent = ["iphone", "ipod", "ipad", "android", "mobile", "blackberry", "webos", "incognito", "webmate", "bada", "nokia", "lg", "ucweb", "skyfire"];
-const browser = typeof window !== 'undefined' ? window.navigator.userAgent.toLowerCase() : navigator.userAgent.toLowerCase();
+const browser = null
+if (typeof window !== 'undefined') {
+    browser = window.navigator.userAgent.toLowerCase();
+}
 let isMobile = false;
-for (let i=0; i<mobileAgent.length; i++)
-{
-    if(browser.indexOf(mobileAgent[i])!=-1)
-    {
+for (let i = 0; i < mobileAgent.length; i++) {
+    if (browser.indexOf(mobileAgent[i]) != -1) {
         isMobile = true;
         break;
     }
